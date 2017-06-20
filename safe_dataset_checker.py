551f506391317fc0f428c59398cf4274451a8b91
davidorme <d.orme@imperial.ac.uk>
2017-06-20 09:56:07 +0100

    Set levels of Messages through the functions

--- safe_dataset_checker.py
+++ safe_dataset_checker.py
@@ -35,6 +35,7 @@ class Messages(object):
         self.messages = []
         self.verbose = verbose
         self.kinds = {'info': '-', 'hint':'?', 'warn':'!'}
+        self.count_new_warnings= 0
         if verbose:
             print(self.header)
 
@@ -44,19 +45,20 @@ class Messages(object):
     # - hint: things we might like differently, but we'll let pass
     # - warn: this is not ok.
 
-    def info(self, message, level=1):
+    def info(self, message, level=0):
         msg = ('info', message, level)
         self.messages.append(msg)
         if self.verbose:
             self.print_msg(*msg)
 
-    def warn(self, message, level=1):
+    def warn(self, message, level=0):
         msg = ('warn', message, level)
         self.messages.append(msg)
+        self.count_new_warnings += 1
         if self.verbose:
             self.print_msg(*msg)
 
-    def hint(self, message, level=1):
+    def hint(self, message, level=0):
         msg = ('hint', message, level)
         self.messages.append(msg)
         if self.verbose:
@@ -66,27 +68,28 @@ class Messages(object):
     # return the text report as a StringIO object
 
     def print_msg(self, kind, msg, level):
-
-        print('  ' * level + self.kinds[kind] + msg)
+        print('  ' * level + self.kinds[kind] + ' ' + msg)
 
     def report(self):
-
         report = StringIO()
         report.writelines([self.header])
-
         msgs = ['  ' * lv + self.kinds[kn] + msg for lv, kn, msg in self.messages]
         report.writelines(msgs)
-
         return report
 
-    # Functions to report errors
+    # Functions to check warnings
+    def new_warnings(self):
+        if self.count_new_warnings:
+            new = self.count_new_warnings
+            self.count_new_warnings = 0
+            return new
+        else:
+            return 0
 
     def count_warnings(self):
-
         return sum([msg[0] == 'warn' for msg in self.messages])
 
     def no_warnings(self):
-
         if self.count_warnings() > 0:
             return False
         else:
@@ -138,31 +141,31 @@ def get_summary(wb, m):
 
     # don't bail here - try and get as far as possible
     if not found.issuperset(required):
-        m.warn('Missing metadata fields: {}'.format(','.join(required - found)))
+        m.warn('Missing metadata fields: {}'.format(','.join(required - found)), 1)
 
     # check for contents
     if any([set(x) == {None} for x in summary_dict.values()]):
-        m.warn('Metadata fields with no information.')
+        m.warn('Metadata fields with no information.', 1)
 
     # CHECK PROJECT ID
     if 'SAFE Project ID' not in summary_dict:
-        m.warn('SAFE Project ID missing')
+        m.warn('SAFE Project ID missing', 1)
         ret_dict['project_id'] = None
     else:
         if type(summary_dict['SAFE Project ID'][0]) != long:
-            m.warn('SAFE Project ID is not an integer.')
+            m.warn('SAFE Project ID is not an integer.', 1)
         ret_dict['project_id'] = summary_dict['SAFE Project ID'][0]
 
     # CHECK DATASET TITLE
     if 'Title' not in summary_dict:
-        m.warn('Dataset title missing')
+        m.warn('Dataset title missing', 1)
         ret_dict['title'] = None
     else:
         ret_dict['title'] = summary_dict['Title'][0]
 
     # CHECK DATASET DESCRIPTION
     if 'Description' not in summary_dict:
-        m.warn('Dataset title missing')
+        m.warn('Dataset title missing', 1)
         ret_dict['description'] = None
     else:
         ret_dict['description'] = summary_dict['Title'][0]
@@ -174,21 +177,21 @@ def get_summary(wb, m):
         ret_dict['embargo_date'] = None
         if access_status == 'Embargo':
             if 'Embargo date' not in summary_dict:
-                m.warn('Dataset embargoed but no date provided.')
+                m.warn('Dataset embargoed but no date provided.', 1)
             embargo_date = summary_dict['Embargo date'][0]
             now = datetime.datetime.now()
             if type(embargo_date) != datetime.datetime:
-                m.warn('Embargo date not formatted as date.')
+                m.warn('Embargo date not formatted as date.', 1)
             elif embargo_date < now:
-                m.warn('Embargo date is in the past.')
+                m.warn('Embargo date is in the past.', 1)
             elif embargo_date > now + datetime.timedelta(days=2*365):
-                m.warn('Embargo date more than two years in the future.')
+                m.warn('Embargo date more than two years in the future.', 1)
             else:
                 ret_dict['embargo_date'] = embargo_date
         elif access_status == 'Open':
             pass
         else:
-            m.warn('Access status must be Open or Embargo not {}'.format(access_status))
+            m.warn('Access status must be Open or Embargo not {}'.format(access_status), 1)
 
     # CHECK AUTHORS
     author_keys  = ['Author name', 'Author affiliation', 'Author email', 'Author ORCID']
@@ -204,19 +207,20 @@ def get_summary(wb, m):
             # look for missing values
             for k, v in au.iteritems():
                 if v is None:
-                    m.warn('Author {} missing'.format(k))
+                    m.warn('Author {} missing'.format(k), 1)
             # check validity
             if not re_name.match(au['name']):
-                m.warn('Author name not formated as last_name, first_names: {}'.format(au['name']))
+                m.warn('Author name not formated as last_name, '
+                       'first_names: {}'.format(au['name']), 1)
             if not re_orcid.match(str(au['orcid'])) or au['orcid'] is None:
-                m.warn('ORCID not properly formatted: {}'.format(au['orcid']))
+                m.warn('ORCID not properly formatted: {}'.format(au['orcid']), 1)
             if not re_email.match(au['email']):
-                m.warn('Email not properly formatted: {}'.format(au['email']))
+                m.warn('Email not properly formatted: {}'.format(au['email']), 1)
             authors[ind] = au
 
         ret_dict['authors'] = authors
     else:
-        m.warn('Author metadata block incomplete.')
+        m.warn('Author metadata block incomplete.', 1)
         ret_dict['authors'] = None
 
     # CHECK DATASETS
@@ -235,17 +239,17 @@ def get_summary(wb, m):
             # look for missing values
             for k, v in dt.iteritems():
                 if v is None:
-                    m.warn('Dataset {} missing'.format(k))
+                    m.warn('Dataset {} missing'.format(k), 1)
             # validate
             if dt['worksheet'] not in valid_sheets:
-                m.warn('Dataset worksheet not found: {}.'.format(dt['worksheet']))
+                m.warn('Dataset worksheet not found: {}.'.format(dt['worksheet']), 1)
             if dt['type'] not in ['Dataframe', 'Matrix']:
-                m.warn('Dataset type not valid: {}.'.format(dt['type']))
+                m.warn('Dataset type not valid: {}.'.format(dt['type']), 1)
             datasets[ind] = dt
 
         ret_dict['datasets'] = datasets
     else:
-        m.warn('Dataset metadata block incomplete.')
+        m.warn('Dataset metadata block incomplete.', 1)
         ret_dict['datasets'] = None
 
     # check for extra undocumented spreadsheets
@@ -254,12 +258,13 @@ def get_summary(wb, m):
                               ['Summary', 'Taxa', 'Locations'])
         if valid_sheets != expected_sheets:
             m.warn('Extra sheets found in workbook: '
-                   '{}'.format(','.join(valid_sheets - expected_sheets)))
+                   '{}'.format(','.join(valid_sheets - expected_sheets)), 1)
 
-    if m.no_warnings():
-        m.info('Summary formatted correctly')
+    new_warn = m.new_warnings()
+    if new_warn:
+        m.info('Summary contains {} errors'.format(new_warn), 1)
     else:
-        m.info('Summary contains {} errors'.format(m.count_warnings()))
+        m.info('Summary formatted correctly', 1)
 
     return ret_dict
 
@@ -272,7 +277,7 @@ def get_locations(wb, m):
         locs = wb['Locations']
     except KeyError:
         # No locations is pretty implausible
-        m.warn("No lcoations worksheet found. Moving on")
+        m.warn("No locations worksheet found - moving on", 1)
         return None
 
     # Check the headers
@@ -285,7 +290,7 @@ def get_locations(wb, m):
 
     # check the key fields are there
     if not set(fields).issuperset({'Location name'}):
-        m.warn('Location name column not found.')
+        m.warn('Location name column not found', 1)
         return None
 
     # get the location names
@@ -295,14 +300,15 @@ def get_locations(wb, m):
 
     # check for duplicate names
     if len(set(loc_names)) != len(loc_names):
-        m.warn('Duplicated location names')
+        m.warn('Duplicated location names', 1)
 
     loc_names = set(loc_names)
 
-    if m.no_warnings():
-        m.info('{} locations loaded correctly'.format(len(loc_names)))
+    new_warn = m.new_warnings()
+    if new_warn:
+        m.info('Locations contains {} errors'.format(m.count_warnings()), 1)
     else:
-        m.info('Locations contains {} errors'.format(m.count_warnings()))
+        m.info('{} locations loaded correctly'.format(len(loc_names)), 1)
 
     return loc_names
 
@@ -325,7 +331,7 @@ def get_taxa(wb, m):
     except KeyError:
         # This might mean that the study doesn't have any taxa, so return an empty
         # set. If the datasets then contain taxonomic names, it'll fail gracefully.
-        m.hint("No taxa worksheet found - assuming no taxa in data for now!")
+        m.hint("No taxa worksheet found - assuming no taxa in data for now!", 1)
         return set()
 
     # Check the headers
@@ -338,7 +344,7 @@ def get_taxa(wb, m):
 
     # check the two key fields are there
     if not set(fields).issuperset({'Taxon name', 'Taxon type'}):
-        m.warn('One or both of Taxon name and Taxon type columns not found')
+        m.warn('One or both of Taxon name and Taxon type columns not found', 1)
         return None
 
     # get the taxon names and types
@@ -350,26 +356,26 @@ def get_taxa(wb, m):
 
     # check for duplicate names
     if len(set(taxon_names)) != len(taxon_names):
-        m.warn('Duplicated taxon names')
+        m.warn('Duplicated taxon names', 1)
 
     # Check for some common taxonomic levels. We want as much taxonomic
     # context as possible, but if all taxa are morphospecies / functional
     # groups to family level then genus and species are just empty columns.
     taxon_levels = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
     if len(set(taxon_types) & set(taxon_levels)) == 0:
-        m.warn('Please provide fields giving at least some of the core taxonomic levels')
+        m.warn('Please provide fields giving at least some of the core taxonomic levels', 1)
 
     # now cross check the given types against the field headers
     types_found = set(taxon_types)
     types_valid = set(fields) - {'Taxon name', 'Taxon type'}
     if not types_found.issubset(types_valid):
         m.warn('Some rows have taxon types that do not match a column name: '
-               '{}'.format(','.join(types_found - types_valid)))
+               '{}'.format(','.join(types_found - types_valid)), 1)
 
     if m.no_warnings():
-        m.info('{} taxa loaded correctly'.format(len(taxon_names)))
+        m.info('{} taxa loaded correctly'.format(len(taxon_names)), 1)
     else:
-        m.info('Taxa contains {} errors'.format(m.count_warnings()))
+        m.info('Taxa contains {} errors'.format(m.count_warnings()), 1)
 
     return set(taxon_names)
 
@@ -403,7 +409,7 @@ def check_dataframe(wb, ws_name, taxa, locations, m):
         field_name_row = descriptors.index('field_name') + 1
         descriptors = descriptors[:field_name_row]
     else:
-        m.warn('Cannot parse data: field_name row not found')
+        m.warn('Cannot parse data: field_name row not found', 1)
         return None
 
     # get the metadata for each field
@@ -422,21 +428,21 @@ def check_dataframe(wb, ws_name, taxa, locations, m):
                 'Continuous', 'Abundance', 'Comments', 'Taxa', None}
 
     if not ft_found.issubset(ft_known):
-        m.warn('Unknown field types: {}'.format(','.join(ft_found - ft_known)))
+        m.warn('Unknown field types: {}'.format(','.join(ft_found - ft_known)), 1)
 
     # check extra descriptors
     if 'Categorical' in ft_found and 'categories' not in descriptors:
-        m.warn('Categorical data fields found but no category descriptions provided.')
+        m.warn('Categorical data fields found but no category descriptions provided.', 1)
 
     if 'Continuous' in ft_found and 'units' not in descriptors:
-        m.warn('Continuous data fields found but no units provided.')
+        m.warn('Continuous data fields found but no units provided.', 1)
 
     # check field names unique (drop None)
     fn_found = Counter([fld['field_name'] for fld in field_metadata
                         if fld['field_name'] is not None])
     duplicated = [k for k, v in fn_found.iteritems() if v > 1]
     if len(duplicated) > 0:
-        m.warn('Field names duplicated: {}'.format(', '.join(duplicated)))
+        m.warn('Field names duplicated: {}'.format(', '.join(duplicated)), 1)
 
     # check mandatory fields
 
@@ -449,7 +455,7 @@ def check_dataframe(wb, ws_name, taxa, locations, m):
             break
 
         # prep the messages instance to pass to functions
-        m.info('Checking field {}'.format(meta['field_name']))
+        m.info('Checking field {}'.format(meta['field_name']), 1)
 
         # read the values
         data_block = ws.get_squared_range(idx + 2, field_name_row + 1, idx + 2, max_row)
@@ -471,9 +477,9 @@ def check_dataframe(wb, ws_name, taxa, locations, m):
             pass
 
     if m.no_warnings():
-        m.info('Dataframe formatted correctly')
+        m.info('Dataframe formatted correctly', 1)
     else:
-        m.info('Dataframe contains {} errors'.format(m.count_warnings()))
+        m.info('Dataframe contains {} errors'.format(m.count_warnings()), 1)
 
     return None
 
@@ -485,12 +491,12 @@ def check_missing_data(meta, data, m):
 
     na_vals = [vl in NA for vl in data]
     if any(na_vals):
-        m.info('{} / {} values missing'.format(sum(na_vals), len(na_vals)))
+        m.info('{} / {} values missing'.format(sum(na_vals), len(na_vals)), 2)
 
     # and then there are non-empty cells containing only whitespace strings
     ws_only = [re_whitespace_only.match(vl) for vl in data if type(vl) in [str, unicode]]
     if any(ws_only):
-        m.warn('{} cells contain whitespace only text'.format(sum(ws_only)))
+        m.warn('{} cells contain whitespace only text'.format(sum(ws_only)), 2)
 
 
 """
@@ -512,15 +518,15 @@ def check_field_datelike(meta, data, m):
     type_check = set([type(vl) for vl in data if vl not in NA])
     if type_check != {datetime.datetime}:
         if datetime.time in type_check:
-            m.warn('Time formatted data found in date field.')
+            m.warn('Time formatted data found in date field.', 2)
         else:
-            m.warn('Non-date formatted data found.')
+            m.warn('Non-date formatted data found.', 2)
 
     # Check no time component in date fields
     if meta['field_type'] == 'Date':
         no_time = [vl.time() == datetime.time(0, 0) for vl in data if vl not in NA]
         if not all(no_time):
-            m.warn('Datetimes found in field of type Date.')
+            m.warn('Datetimes found in field of type Date.', 2)
 
 
 def check_field_time(meta, data, m):
@@ -536,41 +542,36 @@ def check_field_time(meta, data, m):
     # Check type (excluding NA values)
     type_check = set([type(vl) for vl in data if vl not in NA])
     if type_check != {datetime.time}:
-        m.warn('Non-time formatted data found.')
+        m.warn('Non-time formatted data found.', 2)
 
 
 def check_field_categorical(meta, data, m):
 
     # this has already been tested but make it robust
     if 'categories' not in meta:
-        m.abort('Category levels metadata not provided')
-        return m
-
-    # check and parse the category labels
-    if meta['categories'] is None:
-        m.abort('Category descriptions empty')
-        return m
+        m.warn('Category levels metadata not provided', 2)
+    elif meta['categories'] is None:
+        m.warn('Category descriptions empty', 2)
     elif type(meta['categories']) is not unicode:
-        m.abort('Category descriptions are not text')
-        return m
+        m.warn('Category description does not seem to be text', 2)
     else:
         # split the text up by semi-colon
         categories = meta['categories'].split(';')
         # strip off any description after a colon
         category_labels = set([ct.split(':')[0] for ct in categories])
 
-    # get the unique values reported in the data, removing missing values and
-    # then convert to unicode in case anyone has used numeric level names.
-    reported = set(data) - NA
-    reported = {unicode(lv) for lv in reported}
+        # get the unique values reported in the data, removing missing values and
+        # then convert to unicode in case anyone has used numeric level names.
+        reported = set(data) - NA
+        reported = {unicode(lv) for lv in reported}
 
-    # check for completeness
-    if not reported.issubset(category_labels):
-        m.warn('Categories found in data missing from description'
-               '{}: '.format(','.join(reported - category_labels)))
+        # check for completeness
+        if not reported.issubset(category_labels):
+            m.warn('Categories found in data missing from description'
+                   '{}: '.format(','.join(reported - category_labels)), 2)
 
 
-def check_field_continuous(meta, data):
+def check_field_continuous(meta, data, m):
 
     """
     Checks for consistency in time fields
@@ -583,7 +584,7 @@ def check_field_continuous(meta, data):
     # Check type (excluding NA values)
     type_check = set([type(vl) for vl in data if vl not in NA])
     if not type_check.issubset({long, float}):
-        raise RuntimeError('Non numeric data found in {}.'.format(meta['field_name']))
+        m.warn('Non numeric data found in {}.'.format(meta['field_name']), 2)
 
 
 def check_field_taxa(meta, data, m, taxa):
@@ -591,7 +592,8 @@ def check_field_taxa(meta, data, m, taxa):
     # check if taxa are all provided
     found = set(data)
     if not found.issubset(taxa):
-        m.warn('Includes taxa missing from Taxa worksheet: {}'.format(', '.join(found - taxa)))
+        m.warn('Includes taxa missing from Taxa worksheet:'
+               ' {}'.format(', '.join(found - taxa)), 2)
 
 
 def check_file(fname, verbose=True):
