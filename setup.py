7e8f54b032a1a27bb23a7cd89b29315295c20226
davidorme <d.orme@imperial.ac.uk>
2018-12-06 16:38:51 +0000

    Added setup.py to allow package to be installed

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="safe_dataset_checker",
+    version="0.0.1",
+    author="David Orme",
+    author_email="d.orme@imperial.ac.uk",
+    description="A small example package",
+    long_description="# TODO",
+    long_description_content_type="text/markdown",
+    url="https://github.com/imperial_college_london/safe_dataset_checker",
+    #packages=setuptools.find_packages(),
+    py_modules=['safe_dataset_checker'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
