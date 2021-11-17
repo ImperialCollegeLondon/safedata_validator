7cb2f1ca53f222a3f0e493352208ec37a2ff4a33
Jacob Cook <jc2017@ic.ac.uk>
2021-11-17 10:58:07 +0000

    Started genbank taxa module

---
+++ safedata_validator/genb_taxa.py
@@ -0,0 +1,17 @@
+# Bunch of imports and similar
+
+"""
+This module describes classes and methods used to both assess the validity of
+GenBank taxonic data, and to check that the taxonomic data is consistent with
+that stored in GBIF. When a conflict arises the user will be informed and asked
+to reduce the level of taxonomic detail they provide to the point where GenBank
+and GBIF are in agreement.
+
+DETAILS OF STUFF DEFINED.
+
+FURTHER DETAILS OF STUFF DEFINED.
+
+MORE SPIEL (ANYTHING THAT ITS IMPORTANT THAT THE END USER NOTES).
+"""
+
+# NEED TO FIGURE OUT PRECISLY WHAT I AM LOOKING FOR BASICALLY
