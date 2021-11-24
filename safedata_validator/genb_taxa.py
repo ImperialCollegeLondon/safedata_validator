aca366f2f14261660027b729e92c27e06c9e4baf
Jacob Cook <jc2017@ic.ac.uk>
2021-11-24 14:28:05 +0000

    Started function to get full taxa information from ID

--- safedata_validator/genb_taxa.py
+++ safedata_validator/genb_taxa.py
@@ -1,8 +1,12 @@
 from typing import Union, Optional
 import dataclasses
+import requests
+from enforce_typing import enforce_types
 
-# SHOULD I DO THIS OR DO I NEED MORE BACKBONE RANKS???
-from .taxa import BACKBONE_RANKS
+# DELETE THE BELOW WHEN I AM DONE, AND INSTEAD IMPORT FROM TAXA
+BACKBONE_RANKS = ['kingdom', 'phylum', 'order', 'class', 'family',
+                  'genus', 'species', 'subspecies']
+# from safedata_validator.taxa import BACKBONE_RANKS
 
 """
 This module describes classes and methods used to both assess the validity of
@@ -18,102 +22,23 @@ FURTHER DETAILS OF STUFF DEFINED.
 MORE SPIEL (ANYTHING THAT ITS IMPORTANT THAT THE END USER NOTES).
 """
 
-# MAKE A NEW CLASS FOR THE MICROBIAL TAXA, AS THEY NEED DIFFERENT FUNCTIONS + DIFFERENT DATA
-# THINK I SHOULD BE DRAWING DATA FROM https://www.ncbi.nlm.nih.gov/taxonomy
-@enforce_types
-@dataclasses.dataclass
-class MicrobeTaxon:
-    """Holds taxonomic information from a user, which can be populated using GBIF validation.
+class NCBIError(Exception):
+    """Exception class for remote NCBI errors
 
-    There are 4 class properties that can be used to create an instance:
-        * name
-        * rank
-        * genbank_id
-        * ignore_gbnk: a GenBank id match to reject
-    The remaining properties are populated by processing functions not when
-    an instance is created.
-        * is_backbone: the taxon is at a taxonomic level included in the GBIF backbone
-        * is_canon: the taxon is considered canon in GenBank
-        # THESE VALUES PROBABLY CHANGE
-        * lookup_status: the outcome of the lookup with one of the following values:
-          found, no_match, validation_fail, unknown_id, id_mismatch
-        # DON'T KNOW IF THIS WOULD BE RETURNED IN OUR CASE
-        * taxon_status: the taxonomic status of the taxon with one of the following values:
-          accepted, doubtful, synonym etc. etc.
-        # AGAIN UNSURE IF THIS IS SOMETHING WE WANT IN THE NEW CASE
-        * parent_id: a GBIF id for the accepted parent taxon.
-        * canon_usage: a Taxon instance holding the canonical usage for the taxon
-        # WILL NEED TO FIND OUT IF ANY EXTRA INFORMATION IS ACTUALLY PROVIDED
-        * note: a string of any extra information provided by the search
-        # UNSURE ON THIS ONE
-        * hierarchy: a list of 2-tuples of rank and GBIF ID for the taxonomic hierarchy
+    Attributes:
+        message -- explanation of the error
     """
 
-    # Init properties
-    name: str
-    rank: str
-    genbank_id: Optional[Union[int, float]] = None
-    ignore_gbnk: Optional[Union[int, float]] = None
-    is_backbone: bool = dataclasses.field(init=False)
-    is_canon: bool = dataclasses.field(init=False)
-    # DON'T KNOW IF THIS SHOULD BECOME MicrobeTaxon OR NOT
-    canon_usage: 'Taxon' = dataclasses.field(init=False)  # https://stackoverflow.com/questions/33533148
-    parent_id: int = dataclasses.field(init=False)
-    taxon_status: str = dataclasses.field(init=False)
-    lookup_status: str = dataclasses.field(init=False)
-    note: str = dataclasses.field(init=False)
-    hierarchy: list = dataclasses.field(init=False)
-
-    def __post_init__(self):
-        """Sets the defaults for the post-init properties and checks inputs
-        """
-
-        if self.genbank_id is not None:
-            if isinstance(self.genbank_id, float) and not self.genbank_id.is_integer():
-                raise ValueError('GenBank Id is not an integer')
-            self.genbank_id = int(self.genbank_id)
-
-        if self.ignore_gbnk is not None:
-            if isinstance(self.ignore_gbnk, float) and not self.ignore_gbnk.is_integer():
-                raise ValueError('Ignore GenBank is not an integer')
-            self.ignore_gbnk = int(self.ignore_gbnk)
-
-        self.rank = self.rank.lower()
-        self.is_backbone = self.rank in BACKBONE_RANKS
-        self.is_canon = False
-        self.canon_usage = None
-        self.parent_id = None
-        self.taxon_status = None
-        self.lookup_status = 'unvalidated'
-        self.note = None
-        self.hierarchy = []
-
-    def __repr__(self):
-
-        if not self.is_backbone:
-            return f"{self.name} (not of GBIF backbone rank)"
-        elif self.found:
-            if self.is_canon:
-                return f"{self.name}"
-            else:
-                return f"{self.name} ({self.taxon_status})"
-        else:
-            return f"{self.name} (not found: {self.lookup_status})"
-
-    @property
-    def found(self):
-        """Convenience boolean property for processing"""
-        return self.is_backbone and self.lookup_status == 'found'
-
-# IMMEDIATE TASKS
-# 1) WORK OUT HOW TO INTERFACE WITH GENBANK DATABASE
-# 2) WRITE FUNCTION THAT CHECKS IF DATA EXISTS IN GENBANK
+    def __init__(self, message="NCBI taxa ID not found"):
+        self.message = message
+        super().__init__(self.message)
 
 # Meta questions:
 # What do we want to store? GBIF compatable taxonomy? With or without GenBank ID?
 # How do we address newly defined species?
+# Do we want a LocalNCBIValidator
 
-# So we want a GenBank taxonomy to be provided
+# TO DO - So we want a GenBank taxonomy to be provided
 # From name alone we should be able to find an ID
 # But probably worth asking for a taxon ID in case of name conflicts
 # First stage is basically does this exist in GenBank at all
@@ -125,4 +50,81 @@ class MicrobeTaxon:
 
 # TO DO - Validate against GBIF
 # So check if taxa provided exists if GBIF, if not check up hierachy until one that does is found
-# Then tell user that they have to contract their taxonomic specification to this level
+# Then tell user that they have to contract their taxonomic specification to this levels
+
+@enforce_types
+class RemoteNCBIValidator:
+    # ADD MORE COMMENTS HERE AS AND WHEN I FIGURE THEM OUT
+    """This provides a validate method for a MicrobeTaxon using various online
+    NCBI APIs. This doesn't need an __init__ method and just contains methods.
+    """
+    # Functionality to find taxa information from genbank ID
+    def id_lookup(self, genbank_id: int):
+        """Method to return taxonomic information from a GenBank ID. THERE'S PROBABLY MORE TO SAY THOUGH
+
+        Params:
+            genbank_id: An integer
+
+        Returns:
+            DECIDE WHAT I WANT IT TO RETURN
+        """
+
+        if not isinstance(genbank_id, int):
+            raise TypeError()
+
+        if not genbank_id > 0:
+            raise ValueError()
+
+        # Make appropriate url
+        up_url = f"""http://api.unipept.ugent.be/api/v1/taxonomy.json?input[]=
+                 {genbank_id}&extra=true&names=true"""
+
+        # Retrive data using the url
+        taxon_row = requests.get(up_url)
+
+        # Extract the response
+        response = taxon_row.json()
+
+        # Check if list is empty
+        if not response:
+            raise NCBIError()
+
+        return response
+        # NEED TO WORK OUT WHAT TO DO BELOW HERE
+
+        # # Create and populate taxon
+        # taxon = Taxon(name=response['canonicalName'],
+        #               rank=response['rank'].lower(),
+        #               gbif_id=response['nubKey'])
+        #
+        # # First, set the parent key - in the GBIF API, this is always provided
+        # # and always points to the true parent taxon, unlike parent_key in
+        # # the simple local database. Note that parentKey is not included in the
+        # # response for Kingdom level taxa, hence get().
+        # taxon.parent_id = response.get('parentKey')
+        # taxon.taxon_status = response['taxonomicStatus'].lower()
+        # taxon.lookup_status = 'found'
+
+        # # Add the taxonomic hierarchy from the accepted usage - these are tuples
+        # # to be used to extend a set for the taxonomic hierarchy
+        # taxon.hierarchy = [(rk, response[ky])
+        #                    for rk, ky in [(r, r + 'Key') for r in BACKBONE_RANKS[:-1]]
+        #                    if ky in response]
+        #
+        # # Now populate the canon details, which requires another look up if the
+        # # user details are for a synonym etc.
+        # if taxon.taxon_status in ('accepted', 'doubtful'):
+        #     taxon.is_canon = True
+        # else:
+        #     taxon.is_canon = False
+        #     # acceptedKey is not provided in the response for canon taxa.
+        #     taxon.canon_usage = self.id_lookup(response['acceptedKey'])
+        #
+        # return taxon
+
+# Make validator
+val = RemoteNCBIValidator()
+# Look up an ID
+test = val.id_lookup(3333)
+# Print out whatever gets returns
+print(test)
