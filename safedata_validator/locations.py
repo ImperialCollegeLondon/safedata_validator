e094838d4d0aad596a1fb082d324df9c9d433ef2
dorme <d.orme@imperial.ac.uk>
2022-01-19 14:36:14 +0000

    Bug checking and pytest development for Locations.add_new_locations

--- safedata_validator/locations.py
+++ safedata_validator/locations.py
@@ -3,7 +3,7 @@ from shapely import wkt
 from shapely.errors import WKTReadingError
 
 from safedata_validator.logger import LOGGER, FORMATTER, CH, loggerinfo_push_pop
-from safedata_validator.validators import (GetDataFrame, IsLower, IsNotBlank,
+from safedata_validator.validators import (GetDataFrame, IsLower, IsNotBlank, IsNumber,
                                            IsString, HasDuplicates, IsLocName)
 from safedata_validator.extent import Extent
 
@@ -30,11 +30,8 @@ class Locations:
 
         self.locations = set()
         self.location_index = []
-        self.latitudinal_extent = Extent('latitude', (float, int))
-        self.longitudinal_extent = Extent('latitude', (float, int))
-        # self.new = False
-        # self.lonlat = False
-        # self.wkt = False
+        self.extents = {'latitude': Extent('latitude', (float, int)),
+                        'longitude': Extent('longitude', (float, int))}
         self.locations_used = set()
 
         self.valid_locations = resources.valid_locations
@@ -125,131 +122,176 @@ class Locations:
         one of  both `latitude` _and_ `longitude` or `wkt`.
         """
 
-        # Validation
+        # Validation - TODO check locs is a list of dicts 
+        # - Do all the dicts have the same keys
         loc_keys = set([tuple(k.keys()) for k in locs])
         if len(loc_keys) > 1:
+            # TODO - should this be CRITICAL? Check error types.
             LOGGER.error('Inconsistent keys in add_new_locations')
             return
         
+        # - Do they provide location names...
+        loc_keys = loc_keys.pop()
         if 'location name' not in loc_keys:
             LOGGER.error("No location name entries in add_new_locations")
             return
 
-        # - Check for blanks
+        # - ... and are any of those names blank ...
         loc_names = [itm['location name'] for itm in locs]
         loc_names = IsNotBlank(loc_names, keep_failed=False)
         if not loc_names:
             LOGGER.error('Location names contains empty cells or whitespace text')
 
+        # - ... or not strings. Only allow strings for new locations - known 
+        #   locations get a pass for integer site codes but not here.
+        loc_names = IsString(loc_names)
+        if not loc_names:
+            LOGGER.error('New location names include non-string values: ',
+                         extra={'join': loc_names.failed})
+
         # Look for duplicated names in inputs - this includes all types and so
         # could give messy information but can't detect duplicates late on
         # cleaned names because we're dealing with sets by that point and there
-        # can be no duplication
+        # can be no duplication in sets
         dupes = HasDuplicates(loc_names)
         if dupes:
-            LOGGER.error('Added names contain duplicated values: ', 
+            LOGGER.error('New location names contain duplicated values: ', 
                          extra={'join': dupes})
 
         # Look for new names that duplicate known names
         duplicates_existing = [rw['location name'] for rw in locs
-                                if rw['location name'] in self.known_loc_names]
+                               if rw['location name'] in self.known_loc_names]
 
         if duplicates_existing:
-            LOGGER.error('New location names duplicate existing names and aliases: ',
+            LOGGER.error('New location names duplicate known names and aliases: ',
                             extra={'join': duplicates_existing})
 
         # Type is required - used to indicate the kind of location in the absence
         # of any actual geodata
         if 'type' not in loc_keys:
-            LOGGER.error('Type field missing in add_new_locations')
+            LOGGER.error('New locations do not provide the location type')
+            for this_loc in locs:
+                this_loc['type'] = "MISSING"
         else:
             # get lowercase types
             geo_types = set(IsLower([vl['type'] for vl in locs]))
 
             # Handle blanks
-            if None in geo_types:
-                LOGGER.error('Types for new locations contains blank entries.')
-                geo_types -= {None}
+            geo_types = IsNotBlank(geo_types, keep_failed=False)
+            if not geo_types:
+                LOGGER.error('Types for new locations contains blank or whitespace entries.')
 
             # Handle unknown geo types
             valid_geotypes = {'point', 'linestring', 'polygon', 'transect', 'area'}
-            bad_geo_types = geo_types - valid_geotypes
+            bad_geo_types = set(geo_types) - valid_geotypes
             if bad_geo_types:
-                LOGGER.error('Unknown location types: ',
+                LOGGER.error('New locations include unknown location types: ',
                                 extra={'join': bad_geo_types})
             
-        # Any geometry information
+        # Geometry information
         # Record which geom columns are present
         if ('latitude' in loc_keys) ^ ('longitude' in loc_keys):
-            LOGGER.error('Provide both or neither of latitude and longitude')
+            LOGGER.error('New locations should either latitude _and_ longitude or neither')
 
-        lonlat = 'latitude' in loc_keys and 'longitude' in loc_keys
-        wkt = 'wkt' in loc_keys
+        lonlat_provided = 'latitude' in loc_keys and 'longitude' in loc_keys
+        wkt_provided = 'wkt' in loc_keys
+
+        # TODO - supplying both is not an error, and probably shouldn't be. WKT
+        # takes priority in the index when both are present, but not testing for
+        # congruence at present
 
         # Look for geographic data (even if it is just the explicit statement
         # that none is available using NA)
-        if not lonlat or wkt:
-            LOGGER.error('New locations reported: you must provide Lat/Long or WKT')
+        if not (lonlat_provided or wkt_provided):
+            LOGGER.error('New locations reported: you must provide Lat/Long or WKT,'
+                         'using NA explicitly when this data is missing.')
         else:
             # Check Lat Long and WKT using check_field_geo to validate the values,
-            # since this method automatically updates the dataset extents.
+            # since this method automatically updates the dataset extents. 
 
-            if lonlat:
+            if lonlat_provided:
 
                 LOGGER.info('Validating lat / long data')
 
                 for axs in ['latitude', 'longitude']:
-
+                    
+                    # Allow NAs for unknown location points
                     axs_vals = [vl[axs] for vl in locs if vl[axs] != 'NA']
-                    axs_vals = IsNotBlank(axs_vals)
-
+                    axs_vals = IsNotBlank(axs_vals, keep_failed = False)
                     if not axs_vals:
                         LOGGER.error(f'Blank {axs} values for new locations: use NA.')
+                    
+                    # Check for data types _here_ to keep interpretable errors
+                    # TODO - maybe simplify Extent objects to _assume_ types.
+                    #        Probably no but there is some duplication of effort here
+                    axs_vals = IsNumber(axs_vals, keep_failed = False)
+                    if not axs_vals:
+                        LOGGER.error(f'Non-numeric {axs} values for new locations: ',
+                                     extra={'join': axs_vals.failed})
 
-                    # Create an extent object to check values and report extents
-                    self.extents[axs].update(axs_vals)
+                    # Update extents
+                    if axs_vals.values:
+                        self.extents[axs].update(axs_vals)
 
-            if self.wkt:
+            if wkt_provided:
 
                 LOGGER.info('Validating WKT data')
-
+                blank_wkt = []
+                non_string_wkt = []
                 bad_wkt = []
 
-                # Get locs where WKT not explicitly missing
-                wkt_data = [l for l in locs if l['wkt'] != 'NA']
-                wkt_found = [l for l in wkt_data if l['wkt'] is not None]
-
-                if len(wkt_found) < len(wkt_data):
-                    LOGGER.error('WKT field contains blanks for new locations, use NA')
-
-                for this_new_loc in wkt_found:
-
-                    # Run the potential WKT through the parser
-                    try:
-                        this_new_geom = wkt.loads(this_new_loc['wkt'])
-                    except WKTReadingError:
-                        bad_wkt.append(this_new_loc['location name'])
+                for this_new_loc in locs:
+                    
+                    if this_new_loc['wkt'] is None:
+                        blank_wkt.append(this_new_loc['location name'])
+                    elif not isinstance(this_new_loc['wkt'], str):
+                        non_string_wkt.append(this_new_loc['location name'])
+                    elif this_new_loc['wkt'].isspace():
+                        blank_wkt.append(this_new_loc['location name'])
+                    elif this_new_loc['wkt'] == 'NA':
+                        pass
+                    
                     else:
-                        # Is it a valid 2D geom
-                        if not this_new_geom.is_valid or this_new_geom.has_z:
+
+                        # Run the potential WKT through the parser
+                        try:
+                            this_new_geom = wkt.loads(this_new_loc['wkt'])
+                        except WKTReadingError:
                             bad_wkt.append(this_new_loc['location name'])
-                        # Store the extents to check for sensible coordinates
-                        bnds = this_new_geom.bounds
-                        self.extents['latitude'].update([bnds[1], bnds[3]])
-                        self.extents['longitude'].update([bnds[0], bnds[2]])
+                        else:
+                            # Is it a valid 2D geom
+                            if not this_new_geom.is_valid or this_new_geom.has_z:
+                                bad_wkt.append(this_new_loc['location name'])
+                            # Store the extents to check for sensible coordinates
+                            bnds = this_new_geom.bounds
+                            self.extents['latitude'].update([bnds[1], bnds[3]])
+                            self.extents['longitude'].update([bnds[0], bnds[2]])
+
+                if blank_wkt:
+                    LOGGER.error('Blank WKT values for new locations: use NA.')
+
+                if non_string_wkt:
+                    LOGGER.error('WKT values for new location not a string: ',
+                                 extra={'join': non_string_wkt})
 
                 if bad_wkt:
                     LOGGER.error('WKT information badly formatted, not geometrically valid or 3D: ',
                                     extra={'join': bad_wkt})
 
         # new location names
+        # - test for duplicated names to already added values
+        dupes = [lc for lc in loc_names if lc in self.locations]
+        if dupes:
+            LOGGER.error('Location names already added to Location instance: ',
+                         extra={'join': dupes})
+        
         self.locations.update(loc_names)
 
         for this_new_loc in locs:
-
-            if self.wkt and this_new_loc['wkt'] != 'NA':
+            
+            if wkt_provided and this_new_loc['wkt'] != 'NA':
                 geom = this_new_loc['wkt']
-            elif self.lonlat and ((this_new_loc['latitude'] != 'NA') and (this_new_loc['longitude'] != 'NA')):
+            elif lonlat_provided and ((this_new_loc['latitude'] != 'NA') and (this_new_loc['longitude'] != 'NA')):
                 geom = 'Point({longitude} {latitude})'.format(**this_new_loc)
             else:
                 geom = None
--- test/test_locations.py
+++ test/test_locations.py
@@ -37,7 +37,7 @@ def locations_inst(resources_with_local_gbif):
          (ERROR, 'Location names already added to Location instance'),)), 
           ])
 def test_add_known_locations(caplog, locations_inst, 
-                                  known_loc_names, expected_log):
+                             known_loc_names, expected_log):
 
     # Preload site names for testing duplicate capture and remove
     # repeated "Checking known locations" across all test outputs
@@ -46,6 +46,131 @@ def test_add_known_locations(caplog, locations_inst,
 
     # Test the addition of the parameterised values
     locations_inst.add_known_locations(known_loc_names)
+ 
+    assert len(expected_log) == len(caplog.records)
+
+    assert all([exp[0] == rec.levelno 
+                for exp, rec in zip(expected_log, caplog.records)])
+    assert all([exp[1] in rec.message
+                for exp, rec in zip(expected_log, caplog.records)])
+
+
+@pytest.mark.parametrize(
+    'new_loc_dicts,expected_log',
+      [# Valid inputs -  WKT, LL and WKT + LL
+       ([{'location name': 'my new loc',  'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating WKT data'))), 
+       ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': 4,  'longitude': 117}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating lat / long data'))),
+       ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': 4, 'longitude': 117, 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating lat / long data'),
+          (INFO, 'Validating WKT data'))),
+         # Inconsistent keys
+       ([{'location name': 'my new loc2',  'type': 'POINT', 'wkt': 'POINT(117 4)'},
+         {'location name': 'my new loc', 'type': 'POINT', 'latitude': 4,  'longitude': 117}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'Inconsistent keys in add_new_locations'))), 
+         # Bad names
+        ([{'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+          ((INFO, 'Checking new locations'),
+         (ERROR, 'No location name entries in add_new_locations'))), 
+        ([{'location name': None,  'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'Location names contains empty cells or whitespace text'),
+         (INFO, 'Validating WKT data'))), 
+        ([{'location name': '    \t',  'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'Location names contains empty cells or whitespace text'),
+         (INFO, 'Validating WKT data'))),
+        ([{'location name': 123,  'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'New location names include non-string values'),
+         (INFO, 'Validating WKT data'))),
+        ([{'location name': 'my new loc', 'type': 'POINT', 'wkt': 'POINT(117 4)'},
+          {'location name': 'my new loc', 'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'New location names contain duplicated values'),
+         (INFO, 'Validating WKT data'))), 
+        ([{'location name': 'A_1', 'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (ERROR, 'New location names duplicate known names and aliases'),
+         (INFO, 'Validating WKT data'))), 
+        ([{'location name': 'already added', 'type': 'POINT', 'wkt': 'POINT(117 4)'}],
+        ((INFO, 'Checking new locations'),
+         (INFO, 'Validating WKT data'),
+         (ERROR, 'Location names already added to Location instance')
+         )), 
+         # Bad location types
+        ([{'location name': 'my new loc', 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR, 'New locations do not provide the location type'),
+          (INFO, 'Validating WKT data'))),
+        ([{'location name': 'my new loc',  'type': None, 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR,'Types for new locations contains blank or whitespace entries'),
+          (INFO, 'Validating WKT data'))), 
+        ([{'location name': 'my new loc',  'type': '    \n', 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR,'Types for new locations contains blank or whitespace entries'),
+          (INFO, 'Validating WKT data'))),
+        ([{'location name': 'my new loc',  'type': 'POINTY', 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR,'New locations include unknown location types'),
+          (INFO, 'Validating WKT data'))),
+       # Bad LL data
+       ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': 4, 'wkt': 'POINT(117 4)'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR, 'New locations should either latitude _and_ longitude or neither'),
+          (INFO, 'Validating WKT data'))),
+       ([{'location name': 'my new loc',  'type': 'POINT'}],
+         ((INFO, 'Checking new locations'),
+          (ERROR, 'New locations reported: you must provide Lat/Long or WKT'))), 
+        ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': None,  'longitude': 117}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating lat / long data'),
+          (ERROR, 'Blank latitude values for new locations'))),
+        ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': '   \t',  'longitude': 117}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating lat / long data'),
+          (ERROR, 'Blank latitude values for new locations'))),
+        ([{'location name': 'my new loc', 'type': 'POINT', 'latitude': '4',  'longitude': 117}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating lat / long data'),
+          (ERROR, 'Non-numeric latitude values for new locations'))),
+        # Bad WKT data
+        ([{'location name': 'my new loc',  'type': 'POINT', 'wkt': None}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating WKT data'),
+          (ERROR, 'Blank WKT values for new locations: use NA'))), 
+        ([{'location name': 'my new loc',  'type': 'POINT', 'wkt': '\t   '}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating WKT data'),
+          (ERROR, 'Blank WKT values for new locations: use NA'))), 
+        ([{'location name': 'my new loc',  'type': 'POINT', 'wkt': 9}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating WKT data'),
+          (ERROR, 'WKT values for new location not a string'))), 
+        ([{'location name': 'my new loc',  'type': 'POINT', 'wkt': 'POINT(2)'}],
+         ((INFO, 'Checking new locations'),
+          (INFO, 'Validating WKT data'),
+          (ERROR, "ParseException: Expected number but encountered ')'"),  # From shapely.geos
+          (ERROR, 'WKT information badly formatted, not geometrically valid or 3D'))),
+        ])
+def test_add_new_locations(caplog, locations_inst, 
+                           new_loc_dicts, expected_log):
+
+    # Preload site names for testing duplicate capture and remove
+    # repeated "Checking known locations" across all test outputs
+    locations_inst.add_new_locations([{'location name': 'already added', 
+                                       'type': 'POINT', 
+                                       'wkt': 'POINT(117 4)'}])
+    caplog.clear()
+
+    # Test the addition of the parameterised values
+    locations_inst.add_new_locations(new_loc_dicts)
  
     assert len(expected_log) == len(caplog.records)
 
