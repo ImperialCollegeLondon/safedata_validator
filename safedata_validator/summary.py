f3690374bbb1c55f3ca8f1a46bb77caa7f8b8f6f
dorme <d.orme@imperial.ac.uk>
2022-02-25 16:27:02 +0000

    Complete rejig of resources file system to be more expandable

--- safedata_validator/summary.py
+++ safedata_validator/summary.py
@@ -368,7 +368,7 @@ class Summary:
                 for is_doi in pub_doi_re:
                     if is_doi:
                         api_call = f'https://doi.org/api/handles/{is_doi.string[is_doi.end():]}'
-                        r = requests.get(api_call)
+                        r = requests.get(api_call, verify=False)
                         if r.json()['responseCode'] != 1:
                             LOGGER.error(f'DOI not found: {is_doi.string}')
 
