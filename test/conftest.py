da4b73f7712da678f76709c0c7f5829e1a964c09
dorme <d.orme@imperial.ac.uk>
2022-03-01 12:47:08 +0000

    Created log_check function in conftest, test suite for extent.py

--- test/conftest.py
+++ test/conftest.py
@@ -59,6 +59,26 @@ parameterisation is clumsy and complex.
 """
 
 
+def log_check(caplog, expected_log):
+    """
+    This helper function checks that the captured log during a test
+    matches the expected log levels and messages. 
+
+    Arguments:
+        caplog: An instance of the caplog fixture
+        expected_log: An iterable of 2-tuples containing the 
+            log level and message.
+    """
+
+    assert len(expected_log) == len(caplog.records)
+
+    assert all([exp[0] == rec.levelno 
+                for exp, rec in zip(expected_log, caplog.records)])
+    assert all([exp[1] in rec.message
+                for exp, rec in zip(expected_log, caplog.records)])
+
+
+
 """
 The other contents are fixtures that will be available to all test suites.
 
