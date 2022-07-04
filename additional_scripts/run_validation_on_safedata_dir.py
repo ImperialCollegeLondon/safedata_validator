4bb0add5a57b6caeb31449f1ad29be657a846961
Jacob Cook <jc2017@ic.ac.uk>
2022-07-04 16:15:57 +0100

    Reformatted additional scripts [skip ci]

--- additional_scripts/run_validation_on_safedata_dir.py
+++ additional_scripts/run_validation_on_safedata_dir.py
@@ -4,31 +4,38 @@ the directory structure used in the `safedata` R package. This revalidation is
 extremely useful for debugging.
 """
 
-import os
-from glob import glob
-import humanize
 import csv
+import os
 from datetime import datetime
+from glob import glob
+
+import humanize
+
+from safedata_validator.field import Dataset
 
 # HACK - Problem here with circular imports - need to load locations first
 #        but more broadly need to fix the structure!
 from safedata_validator.locations import Locations
-from safedata_validator.field import Dataset
-from safedata_validator.logger import COUNTER_HANDLER, CONSOLE_HANDLER, LOG
+from safedata_validator.logger import CONSOLE_HANDLER, COUNTER_HANDLER, LOG
 from safedata_validator.resources import Resources
 
-test_files = glob('/Users/dorme/Research/SAFE/Database/safedata_directory/*/*/*.xlsx', recursive=True)
-resources = Resources('/Users/dorme/Research/SAFE/Database/safedata_validator_package/local_db/safedata_validator.cfg')
+test_files = glob(
+    "/Users/dorme/Research/SAFE/Database/safedata_directory/*/*/*.xlsx", recursive=True
+)
+resources = Resources(
+    "/Users/dorme/Research/SAFE/Database/safedata_validator_package/local_db/"
+    "safedata_validator.cfg"
+)
 
-COUNTER_HANDLER.setLevel('ERROR')
-CONSOLE_HANDLER.setLevel(100) # Muted
+COUNTER_HANDLER.setLevel("ERROR")
+CONSOLE_HANDLER.setLevel(100)  # Muted
 
 files = []
 errors = {}
 
 # Loop over the files
 for idx, this_file in enumerate(test_files):
-    
+
     # Record the start of processing
     time_in = datetime.now()
 
@@ -47,15 +54,14 @@ for idx, this_file in enumerate(test_files):
     filesize = os.path.getsize(this_file)
     proc_time = (time_out - time_in).total_seconds()
 
-    print(f'{idx} {humanize.naturalsize(filesize)} {proc_time} {this_file}')
+    print(f"{idx} {humanize.naturalsize(filesize)} {proc_time} {this_file}")
 
     files.append((this_file, filesize, proc_time))
 
 
-
-with open('testing.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+with open("testing.csv", "w", newline="") as csvfile:
+    spamwriter = csv.writer(
+        csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
+    )
     for rw in files:
         spamwriter.writerow(rw)
-
