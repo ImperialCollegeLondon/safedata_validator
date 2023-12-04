9ca6533cd74abc6e76e9843dc481b219e7a2f448
David Orme <davidorme@users.noreply.github.com>
2023-12-04 15:54:11 +0000

    Updating safedata_server to safedata_metadata

--- docs/data_managers/command_line_tools/command_line_usage/update_command_line_usage.sh
+++ docs/data_managers/command_line_tools/command_line_usage/update_command_line_usage.sh
@@ -28,16 +28,16 @@ do
     safedata_zenodo $subc -h >> safedata_zenodo_$subc.txt
 done
 
-echo "cl_prompt $ safedata_server -h" > safedata_server_top.txt
-safedata_server -h >> safedata_server_top.txt
+echo "cl_prompt $ safedata_metadata -h" > safedata_metadata_top.txt
+safedata_metadata -h >> safedata_metadata_top.txt
 
 subcommands=(post_metadata update_gazetteer)
 
 for subc in "${subcommands[@]}";
 do
     echo $subc
-    echo "cl_prompt $ safedata_server $subc -h" > safedata_server_$subc.txt
-    safedata_server $subc -h >> safedata_server_$subc.txt
+    echo "cl_prompt $ safedata_metadata $subc -h" > safedata_metadata_$subc.txt
+    safedata_metadata $subc -h >> safedata_metadata_$subc.txt
 done
 
 echo "cl_prompt $ safedata_build_local_gbif -h" > safedata_build_local_gbif.txt
