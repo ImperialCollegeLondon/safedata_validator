864c66c5d36a989786fd4fa7ad22bb193d2c3054
Jacob Cook <jc2017@ic.ac.uk>
2024-04-29 14:13:06 +0100

    Added a hook printing hello world on docs pre build

---
+++ docs/scripts/hooks.py
@@ -0,0 +1,7 @@
+"""TODO - Add a proper docstring here."""
+
+
+def on_pre_build(config, **kwargs) -> None:
+    """TODO - Add a docstring here."""
+    # THINK I SHOULD LOOK INTO
+    print("Hello world.")
