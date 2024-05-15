296c8f613f7ea732ad9ad7d2e844ce5a1060fd88
Jacob Cook <jc2017@ic.ac.uk>
2024-05-15 10:46:32 +0100

    Updated taxonomy to so that GBIF part of example file passes validation

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -24,8 +24,16 @@ def test_DataSet_load_from_file(fixture_resources, file_key, n_errors):
     assert ds.n_errors == n_errors
 
 
-# TODO - Work out if this requires a specific database version
-def test_Example_dataset(fixture_resources):
+def test_example_dataset(fixture_resources):
+    """Test that the example dataset we provide actually passes validation.
+
+    It is important to note that this will only pass if the truncated databases are made
+    using the database versions stated in their respective *_database_truncator.py
+    scripts. Otherwise changes to the reference taxonomy could cause this test to fail.
+
+    This test will also fail every two years when the embargo date in the example files
+    comes around.
+    """
     # Load the taxa and locations
     ds = Dataset(fixture_resources)
     ds.load_from_workbook(FIXTURE_FILES.rf["example_file"])
