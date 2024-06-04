df6e8982458daf487101b4ea4d41a69939591b20
Jacob Cook <jc2017@ic.ac.uk>
2024-06-04 16:01:17 +0100

    Wrote gazetteer generation example script in R

---
+++ docs/data_managers/install/create_gazetteer.r
@@ -0,0 +1,29 @@
+library(sf)
+library(geojsonio)
+
+# Create points, lines, and polygons
+sampling_point <- st_point(c(117.900, 4.250))
+transect <- st_linestring(
+  cbind(c(117.900, 117.902, 117.904, 117.906), c(4.250, 4.251, 4.252, 4.253))
+)
+sampling_area <- st_polygon(list(cbind(
+  c(117.900, 117.900, 117.855, 117.855, 117.900),
+  c(4.250, 4.255, 4.255, 4.250, 4.250)
+)))
+
+# Combine into a single geometry collection with name attributes defined
+# (coordinate reference system must be 4326 for GeoJSON)
+geom_coll <- st_sfc(sampling_point, transect, sampling_area, crs = 4326)
+manual_locations <- st_sf(
+  name = c("point_A1", "transect_B57", "plot_C324"),
+  geometry = geom_coll
+)
+
+# Load existing shapefile
+shape_file <- st_read("example_shape_file.shp")
+
+# Combine points from different sources
+all_locations <- rbind(manual_locations, shape_file)
+
+# Export as GeoJSON
+geojson_write(all_locations, file = "gazetteer.geojson", na = "null")
