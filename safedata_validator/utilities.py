4aad3146025d010189d8396dc946cbf1be0c9350
Jacob Cook <jc2017@ic.ac.uk>
2024-08-29 11:32:15 +0100

    Added utility functions to determine if a file is zenodo or metadata json

--- safedata_validator/utilities.py
+++ safedata_validator/utilities.py
@@ -8,6 +8,8 @@ check that the correct inputs have been supplied in the correct order.
 import mimetypes
 from pathlib import Path
 
+import simplejson
+
 
 def check_file_is_excel(file_path: Path) -> bool:
     """Check that the file path provided points to a .xlsx file.
@@ -55,5 +57,60 @@ def check_file_is_json(file_path: Path) -> bool:
         return False
 
 
-# TODO - Function to distinguish dataset from zenodo json files (think this requires
-# looking at tags)
+def check_file_is_zenodo_json(file_path: Path) -> bool:
+    """Function to check if a file is a Zenodo JSON file.
+
+    Args:
+        file_path: A path to the file to check
+
+    Returns:
+        A bool specifying whether or not the file is a Zenodo JSON file.
+    """
+
+    # First check that the file is actually JSON
+    if not check_file_is_json(file_path):
+        return False
+
+    # Minimal set of keys to identify zenodo JSON
+    required_keys = ["created", "modified", "conceptrecid", "metadata"]
+
+    with open(file_path) as file:
+        contents = simplejson.load(file)
+
+        # Check if all required keys are present
+        missing_keys = [key for key in required_keys if key not in contents]
+
+        if missing_keys:
+            return False
+        else:
+            return True
+
+
+def check_file_is_metadata_json(file_path: Path) -> bool:
+    """Function to check if a file is a safedata metadata style JSON file.
+
+    Args:
+        file_path: A path to the file to check
+
+    Returns:
+        A bool specifying whether or not the file is a safedata metadata style JSON
+        file.
+    """
+
+    # First check that the file is actually JSON
+    if not check_file_is_json(file_path):
+        return False
+
+    # Minimal set of keys to identify metadata JSON
+    required_keys = ["project_ids", "authors", "access", "funders"]
+
+    with open(file_path) as file:
+        contents = simplejson.load(file)
+
+        # Check if all required keys are present
+        missing_keys = [key for key in required_keys if key not in contents]
+
+        if missing_keys:
+            return False
+        else:
+            return True
--- test/test_utilities.py
+++ test/test_utilities.py
@@ -37,3 +37,40 @@ def test_check_file_is_json(file_path, expected_result):
     is_file_json = check_file_is_json(file_path)
 
     assert is_file_json == expected_result
+
+
+# PROVIDE EXCEL FILE AND ALL THREE JSON FILES, NEED TO TRACK DOWN NAMES
+@pytest.mark.parametrize(
+    "file_path,expected_result",
+    [
+        (Path(FIXTURE_FILES.rf.good_ncbi_file_dataset_json), False),
+        (Path(FIXTURE_FILES.rf.good_ncbi_file_zenodo_json), True),
+        (Path(FIXTURE_FILES.rf.json_not_locations), False),
+        (Path(FIXTURE_FILES.rf.good_excel_file), False),
+    ],
+)
+def test_check_file_is_zenodo_json(file_path, expected_result):
+    """Test that the function to check if a file is JSON works."""
+    from safedata_validator.utilities import check_file_is_zenodo_json
+
+    is_file_json = check_file_is_zenodo_json(file_path)
+
+    assert is_file_json == expected_result
+
+
+@pytest.mark.parametrize(
+    "file_path,expected_result",
+    [
+        (Path(FIXTURE_FILES.rf.good_ncbi_file_dataset_json), True),
+        (Path(FIXTURE_FILES.rf.good_ncbi_file_zenodo_json), False),
+        (Path(FIXTURE_FILES.rf.json_not_locations), False),
+        (Path(FIXTURE_FILES.rf.good_excel_file), False),
+    ],
+)
+def test_check_file_is_metadata_json(file_path, expected_result):
+    """Test that the function to check if a file is JSON works."""
+    from safedata_validator.utilities import check_file_is_metadata_json
+
+    is_file_json = check_file_is_metadata_json(file_path)
+
+    assert is_file_json == expected_result
